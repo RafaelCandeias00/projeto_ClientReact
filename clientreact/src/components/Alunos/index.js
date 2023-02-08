409137e0bdea1f43054265284dc14fb258d75df0
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2023-02-08 15:54:41 -0300

    @RafaelCandeias00 [Task: codificação de relação de alunos e logout | componente Alunos]

--- clientreact/src/components/Alunos/index.js
+++ clientreact/src/components/Alunos/index.js
@@ -1,50 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
-import {Link} from 'react-router-dom';
-import {FiXCircle, FiEdit, FiUserX} from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
+import { FiXCircle, FiEdit, FiUserX } from 'react-icons/fi';
+import api from "../../services/api";
 
-export default function Alunos(){
-    return(
+export default function Alunos() {
+
+    const [nome, setNome] = useState('');
+    const [alunos, setAlunos] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    useEffect(() => {
+        api.get('/alunos', authorization).then(
+            response => {
+                setAlunos(response.data);
+            }, token)
+    })
+
+    async function logout(){
+        try{
+            localStorage.clear();
+            localStorage.setItem('token', '');
+            authorization.headers = '';
+            navigate('/');
+        }catch(error){
+            alert('Não foi possível fazer o logout' + error)
+        }
+    }
+
+    const navigate = useNavigate();
+
+    return (
         <div className='aluno-container'>
             <header>
-                <span>Bem vinde, <strong>Rafael</strong>!</span>
+                <span>Bem vinde, <strong>{email}</strong>!</span>
                 <Link className='button' to="/aluno/novo/0">Novo Aluno</Link>
-                <button type="button">
-                    <FiXCircle size={35} color="#17202a"/>
+                <button onClick={logout} type="button">
+                    <FiXCircle size={35} color="#17202a" />
                 </button>
             </header>
             <form>
-                <input type='text' placeholder='Nome'/>
+                <input type='text' placeholder='Nome' />
                 <button type='button' class='button'>
                     Filtrar aluno por nome (parcial)
                 </button>
             </form>
             <h1>Relação de Alunos</h1>
             <ul>
-                <li>
-                    <b>Nome:</b>Rafael<br/><br/>
-                    <b>Email:</b>rafael@email.com<br/><br/>
-                    <b>Idade:</b>23<br/><br/>
+                {alunos.map(aluno => (
+                    <li key={aluno.id}>
+                        <b>Nome:</b>{aluno.nome}<br /><br />
+                        <b>Email:</b>{aluno.email}<br /><br />
+                        <b>Idade:</b>{aluno.idade}<br /><br />
 
-                    <button type="button">
-                        <FiEdit size={25} color="#17202a"/>
-                    </button>
-                    <button type="button">
-                        <FiUserX size={25} color="#17202a"/>
-                    </button>
-                </li>
-                <li>
-                    <b>Nome:</b>Rafael<br/><br/>
-                    <b>Email:</b>rafael@email.com<br/><br/>
-                    <b>Idade:</b>23<br/><br/>
-
-                    <button type="button">
-                        <FiEdit size={25} color="#17202a"/>
-                    </button>
-                    <button type="button">
-                        <FiUserX size={25} color="#17202a"/>
-                    </button>
-                </li>
+                        <button type="button">
+                            <FiEdit size={25} color="#17202a" />
+                        </button>
+                        <button type="button">
+                            <FiUserX size={25} color="#17202a" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
